0c217eb519a594a1b96ae721567c491dddec42fe
lukasz szczurek <lukaszszczurek@itboom.pl>
2023-09-14 18:39:51 +0200

    home user API works

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+
 function Home() {
     //  get  respose from path /api/v1/users/{email}
     const url = 'http://localhost:8080/api/v1/users/';
@@ -15,8 +16,9 @@ function Home() {
     }
 
     const tokenData = getTokenData();
-    console.log(tokenData.sub);
+    console.log(tokenData.sub + "xd");
     const [data, setData] = React.useState<any>("");
+    const [email, setEmail] = React.useState<string>("a@.pl");
 
     const getUserData = async (url : string) : Promise <any> => {
         console.log(TOKEN)
@@ -24,23 +26,30 @@ function Home() {
 
             // add webSocket connection
             'Access-Control-Allow-Headers': '*','Allow': '*','Access-Control-Allow-Methods': '*','Access-Control-Allow-Credentials': 'true','Access-Allow-Control-Origin': '*'
-            }}).then(response => response.data).catch(error => console.log("pip"));
+            }}).then(response => setData(response.data)).catch(error => console.log("pip"));
     }
 
 
 
     useEffect(() => {
 
-            getUserData(url).then(response => setData(response.data)).catch(error => console.log("useEffect isue"));
 
+            getUserData(url).then(response => setData(response.data)).catch(error => console.log("useEffect isue"));
+        console.log(data.email + "data")
+
+           // setEmail(data.email);
+      
     }, [data]);
 
     return (
         <div>
             <h1>Home</h1>
-            <h2>{TOKEN}</h2>
-            <h2>{data.email}</h2>
-            {/*<h2>{data}</h2>*/}
+
+            <h2> {data.email}</h2>
+
+            <img style={{"height":"10%", "width":"10%" }} src={data.photoUrl} alt="xd" />
+            {/*4*/}
+            {/*<h2>{email}</h2>*/}
         </div>
     );
 }
